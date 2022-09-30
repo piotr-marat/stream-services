6e57e1e391c0ff86a3ea95245adc2dc9301416ef
Piotr Marat <piotr@backbase.com>
2022-09-30 23:43:11 +0200

    WIP.

--- stream-compositions/services/product-composition-service/src/main/java/com/backbase/stream/compositions/product/core/service/impl/ProductPostIngestionServiceImpl.java
+++ stream-compositions/services/product-composition-service/src/main/java/com/backbase/stream/compositions/product/core/service/impl/ProductPostIngestionServiceImpl.java
@@ -110,7 +110,7 @@ public class ProductPostIngestionServiceImpl implements ProductPostIngestionServ
 
     private Mono<ProductIngestResponse> ingestTransactionsAsync(ProductIngestResponse res) {
         Timer timer = registry.timer("ingestion.process.pull.user.transactions",
-                "user-id", res.getUserExternalId());
+                "user-id", "user-name");
 
         return extractProducts(res.getProductGroups())
                 .map(product -> buildTransactionPullRequest(product, res))
